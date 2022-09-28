e8bc771e2a31ed6f7f08fee8be9a8897be8d328a
liuss7 <liuss7@asiainfo.com>
2022-09-28 20:25:34 +0800

    🥰让坦克动起来

--- src/model/tank.ts
+++ src/model/tank.ts
@@ -5,7 +5,7 @@ import _ from "lodash"
 import config from "../config";
 //草地的类
 export default class Tank extends modelAbstract implements IModel {
-  name: string="tank";
+  name: string = "tank";
   //坦克方向
   protected direction: diretionEnum = diretionEnum.bottom
 
@@ -13,10 +13,33 @@ export default class Tank extends modelAbstract implements IModel {
     this.randomDirection()
     // const img=image.get('tank')!
     super.draw(this.randomImage())
+    setInterval(() => {
+      this.move()
+    }, 50)
   }
   //随机产生方向
   randomDirection() {
     this.direction = Object.keys(diretionEnum)[Math.floor(Math.random() * 4)] as diretionEnum
+
+  }
+  //让坦克运动
+  protected move(): void {
+    this.canvas.clearRect(this.x,this.y,config.model.width,config.model.height)
+    switch (this.direction) {
+      case diretionEnum.top:
+        this.y += 2
+        break
+      case diretionEnum.right:
+        this.x += 2
+        break
+      case diretionEnum.bottom:
+        this.y -= 2
+        break
+      case diretionEnum.left:
+        this.x -= 2
+        break
+    }
+    super.draw(this.randomImage())
 
   }
   randomImage() {
